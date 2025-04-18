82ebecdcd29cda4e1ba360d8c1a5c6d3d27137d4
mvass <mvass.pe@gmail.com>
2025-04-18 17:10:16 +0300

    server logic 1

--- server.py
+++ server.py
@@ -2,15 +2,52 @@ import socket
 import json
 import threading
 import os
+from db import MiniDB
 
 IMAGES_FOLDER = 'images'
 IMAGES_DB = 'images.txt'
 RATINGS_DB = 'ratings.txt'
 
+images = MiniDB(['id','path'], IMAGES_DB)
+RATINGS_DB = MiniDB(['ip','image','rating'], RATINGS_DB)
+
+CLEAN_ON_START = False
+if CLEAN_ON_START:
+    open(IMAGES_DB, 'w').close()
+    open(RATINGS_DB, 'w').close()
+
+def scan_image(path):
+    result = []
+    for p in os.listdir(path):
+        full_path = os.path.join(path, p)
+        if (p.lower().endswith('.png') or p.lower().endswith('.jpg')) and os.path.isfile(full_path):
+            result.append(full_path)
+    return full_path
+
+if not images.data:
+    image_list = scan_image(IMAGES_FOLDER)
+    id = 1
+    for img_path in image_list:
+        images.add((id, img_path))
+        id +=1
+images.save_to_file(IMAGES_DB)
+
+user_positions = {}
+
 HOST, PORT = "0.0.0.0", 9999
 
-commands = {}
 
+
+def update_user_position(user, shift, default_value):
+    total = len(images.data)
+    current = user_positions.get(user, default_value)
+    new_index = (current + shift)% total
+    return new_index
+
+def get_image_response(user, record, action):
+    pass
+
+commands = {}
 def command(action):
     def decorator(func):
         commands[action] = func
@@ -19,7 +56,15 @@ def command(action):
 
 @command("get_next")
 def cmd_next(user, request):
-    pass
+    index = update_user_position(user, 1, 0)
+    record = images.data[index]
+    return get_image_response(user, record, "get_next")
+
+@command("get_prev")
+def cmd_prev(user, request):
+    index = update_user_position(user, -1, 0)
+    record = images.data[index]
+    return get_image_response(user, record, "get_prev")
 
 @command("rate")
 def cmd_rate(user, request):
